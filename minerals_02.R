8e361fe9a74c5e2ae564394f67a86775a9cf7d57
Jim Moraga <jmoraga@mines.edu>
2021-05-10 18:06:07 -0600

    Created post-processing including sigmoid normalizatio (with 2 sigmas)

--- minerals_02.R
+++ minerals_02.R
@@ -3,7 +3,10 @@ source("utils/doe_mineral_utils.R")
 minerals_directory <- "../../doe_test/HyMapTargetDetectionFull"
 band_names <- c("OpalizedTuff", "KrattOpal", "Epsomite", "Gypsum", 
                 "Hematite", "Kaolinite", "Chalcedony")
-target_list <- c("Chalcedony", "Kaolinite", "Gypsum", "Epsomite")
+target_list <- c("Chalcedony", "OpalizedTuff", "Kaolinite", "Gypsum", 
+                 "Hematite", "Epsomite", "KrattOpal")
+target_list2 <- c("Chalcedony", "Kaolinite", "Gypsum", 
+                 "Hematite", "Epsomite")
 
 input_files <- c("HyMap_full_ace", "HyMap_full_cem", "HyMap_full_mf",
                  "HyMap_full_osp", "HyMap_full_sam_rule","HyMap_full_tcimf")
@@ -11,13 +14,15 @@ input_files <- c("HyMap_full_ace", "HyMap_full_cem", "HyMap_full_mf",
 all_stacks <- vector(mode="list", length = 0)
 for (file_name in input_files){
   s <- stack(file.path(minerals_directory, file_name))
+  s <- raster::crop(s, extent_desert)
   names(s) <- band_names
   s <- s[[target_list]]
-  s <- raster::setMinMax(s)
   n <- is.na(s)
-  if(grepl("sam_rule", file_name, fixed = TRUE, ignore.case = TRUE)){
-    print(paste0(file_name, " is SAM"))
-    s <- abs(s)
+  s[is.na(s)] <- 0
+  s <- raster::setMinMax(s)
+  if(grepl("sam_rule", file_name, fixed = TRUE)){
+    cat(paste0(file_name, " is SAM\n"))
+    s <- abs(s-1)
     s[s>1]  <- NA
     s <- 1-s
   }
@@ -25,23 +30,72 @@ for (file_name in input_files){
   s_max <- raster::cellStats(s, max)
   s <- (s-s_min)/s_max
   s[n] <- NA
+  names(s) <- target_list
   all_stacks <- append(all_stacks, s)
 }
 
-chalcedony <- stack()
-for (i in 1:length(all_stacks))
-  chalcedony <- stack(chalcedony, all_stacks[[i]][["Chalcedony"]])
-chalcedony <- calc(chalcedony, sum, na.rm = TRUE)
-chalcedony[is.na(all_stacks[[3]][["Chalcedony"]])] <- NA
-plot(chalcedony)
-ace_stack <- stack("results/filtered/ace_stack")
-cem_stack <- stack("results/filtered/cem_stack")
-mf_stack <- stack("results/filtered/mf_stack")
-osp_stack <- stack("results/filtered/osp_stack")
-sam_stack <- stack("results/filtered/sam_stack")
-tcimf_stack <- stack("results/filtered/tcimf_stack")
+# chalcedony <- stack()
+# for (i in 1:length(all_stacks))
+#   chalcedony <- stack(chalcedony, all_stacks[[i]][["Chalcedony"]])
+# chalcedony <- calc(chalcedony, sum, na.rm = TRUE)
+# chalcedony[is.na(all_stacks[[5]][["Chalcedony"]])] <- NA
+# plot(chalcedony)
 
-chalcedony <- stack(sam_stack[["Chalcedony"]], ace_stack[["Chalcedony"]], cem_stack[["Chalcedony"]], mf_stack[["Chalcedony"]], osp_stack[["Chalcedony"]], tcimf_stack[["Chalcedony"]])
-chalcedony <- calc(chalcedony, sum, na.rm = TRUE)
+minerals_stack <- stack()
+for (mineral_name in target_list){
+  m <- stack()
+  for (i in 1:length(all_stacks))
+    m <- stack(m, all_stacks[[i]][[mineral_name]])
+  m <- calc(m, sum, na.rm = TRUE)
+  names(m) <- mineral_name
+  minerals_stack <- stack(minerals_stack, m)
+}
 
-f1 <- doe_write_raster(chalcedony, "results/filtered/Brady_Chalcedony")
+plot(minerals_stack)
+
+hydrothermal <- calc(minerals_stack, sum, na.rm = TRUE)
+names(hydrothermal) <- "Hydrothermal"
+plot(hydrothermal)
+
+minerals_stack2 <- minerals_stack[[target_list2]]
+hydrothermal2 <- calc(minerals_stack2, sum, na.rm = TRUE)
+names(hydrothermal2) <- "Geothermal"
+plot(hydrothermal2)
+
+geo_m <- as.matrix(hydrothermal2)*1000
+mode(geo_m) <- "integer"
+geo<-autothresholdr::auto_thresh(geo_m, method = "Otsu",
+                                 ignore_black = TRUE, ignore_na = TRUE)
+geo/1000.0
+
+sigmoid_normalizer <- function(x){
+  x_mean <- raster::cellStats(x, stat=mean)
+  x_std <-  raster::cellStats(x, stat=sd)
+  x <- 1/(1+exp((x_mean-x)/(2*x_std)))
+  return(x)
+}
+geo2 <- sigmoid_normalizer(hydrothermal2)
+plot(geo2)
+
+geo2_m <- as.matrix(geo2)*1000
+mode(geo2_m) <- "integer"
+geo2_th<-autothresholdr::auto_thresh(geo2_m, method = "Otsu",
+                                 ignore_black = TRUE, ignore_na = TRUE)/1000.0
+cat(paste0("New threshold: ", geo2_th, "\n"))
+geo2[geo2<as.numeric(geo2_th)] <- NA
+geo2 <- unit_normalization(geo2)
+plot(geo2, main="Geothermal alterations after normalization and thresholding", col=rev(heat.colors(100)))
+
+f1 <- doe_write_raster(geo2, "results/filtered/DesertPeak_Markers")
+
+# ace_stack <- stack("results/filtered/ace_stack")
+# cem_stack <- stack("results/filtered/cem_stack")
+# mf_stack <- stack("results/filtered/mf_stack")
+# osp_stack <- stack("results/filtered/osp_stack")
+# sam_stack <- stack("results/filtered/sam_stack")
+# tcimf_stack <- stack("results/filtered/tcimf_stack")
+# 
+# chalcedony <- stack(sam_stack[["Chalcedony"]], ace_stack[["Chalcedony"]], cem_stack[["Chalcedony"]], mf_stack[["Chalcedony"]], osp_stack[["Chalcedony"]], tcimf_stack[["Chalcedony"]])
+# chalcedony <- calc(chalcedony, sum, na.rm = TRUE)
+# 
+# f1 <- doe_write_raster(chalcedony, "results/filtered/Brady_Chalcedony")
